5a8705d21d64d8083c64c1b71d36af2fd8d2c669
Shaurya <shauryamohan55@gmail.com>
2023-08-24 19:14:14 +0530

    Uploading new files to the repo

---
+++ script.js
@@ -0,0 +1,16 @@
+const userForm = document.getElementById("userForm");
+userForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+
+const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const phone = document.getElementById("tele").value;
+const date = document.getElementById("date").value;
+const time = document.getElementById("time").value;
+console.log(name, );
+console.log(email);
+console.log(phone);
+console.log(date);
+console.log(time);
+});
